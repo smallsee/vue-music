4a3e8b16270587b7c557a3d1332f731a884a829f
小海 <xiaohai@xiaohaideMacBook-Pro.local>
2017-07-20 16:42:05 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div>sss</div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="stylus" ref="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  #app
+      color: $color-theme
+</style>
