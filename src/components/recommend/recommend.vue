ba7a4273ade16f3930c59026b7bcc98a674e6e14
小海 <xiaohai@xiaohaideMacBook-Pro.local>
2017-07-21 00:32:16 +0800

    首页弄好

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,25 +1,42 @@
 <template>
     <div class="recommend">
-      <div class="recommend-content">
-        <div v-if="recommends.length"  class="slider-wrapper">
-          <slider>
-            <div v-for="item in recommends">
-              <a :href="item.linkUrl">
-                <img :src="item.picUrl" alt="">
-              </a>
-            </div>
-          </slider>
+      <scroll ref="scroll" class="recommend-content" :data="discList">
+        <div>
+          <div v-if="recommends.length"  class="slider-wrapper">
+            <slider>
+              <div v-for="item in recommends">
+                <a :href="item.linkUrl">
+                  <img  @load="loadImage"  :src="item.picUrl" alt="">
+                </a>
+              </div>
+            </slider>
+          </div>
+          <div class="recommend-list">
+            <h1 class="list-title">热门歌单推荐</h1>
+            <ul>
+              <li  v-for="item in discList" class="item">
+                <div class="icon">
+                  <img width="60" height="60" v-lazy="item.imgurl">
+                </div>
+                <div class="text">
+                  <h2 class="name" v-html="item.creator.name"></h2>
+                  <p class="desc" v-html="item.dissname"></p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="recommend-list">
-          <h1 class="list-title">热门歌单</h1>
-          <ul></ul>
+        <div class="loading-container" v-show="!discList.length">
+          <loading></loading>
         </div>
-      </div>
+      </scroll>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
   import Slider from 'base/slider/slider'
+  import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
   import {getRecommend, getDiscList} from 'api/recommend'
   import {ERR_OK} from 'api/config'
 
@@ -47,9 +64,17 @@
             this.discList = res.data.list
           })
         },
+        loadImage() {
+          if (!this.checkloaded) {
+            this.checkloaded = true;
+            this.$refs.scroll.refresh()
+          }
+        }
       },
       components: {
-        Slider
+        Slider,
+        Scroll,
+        Loading
       }
     }
 </script>
